78d2a8f8add9a123a3ea59c5ecaf935c16b44aeb
Micah Stubbs <micah.stubbs@gmail.com>
2017-11-04 14:06:53 -0700

    update MainActivity.java

--- android/app/src/main/java/com/curiousapp/MainActivity.java
+++ android/app/src/main/java/com/curiousapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.curiousapp;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+import android.content.Intent;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class MainActivity extends ReactActivity {
+
+    private ReactNativePushNotificationPackage mReactNativePushNotificationPackage;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -12,4 +21,35 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "curiousapp";
     }
+
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
+
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+        mReactNativePushNotificationPackage = new ReactNativePushNotificationPackage(this);
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            mReactNativePushNotificationPackage
+        );
+    }
+
+    // Add onNewIntent
+    @Override
+    protected void onNewIntent (Intent intent) {
+      super.onNewIntent(intent);
+
+      mReactNativePushNotificationPackage.newIntent(intent);
+    }
+
 }
