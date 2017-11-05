59d8d47c8f2383095a3702d46d29bdd65e07665a
Micah Stubbs <micah.stubbs@gmail.com>
2017-11-04 20:02:27 -0700

    store data with async storage

--- app/index.js
+++ app/index.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Picker, AppState, Platform } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Picker,
+  AppState,
+  Platform,
+  TextInput,
+  AsyncStorage
+} from 'react-native';
 import PushController from './src/components/PushController';
 import PushNotification from 'react-native-push-notification';
 
@@ -8,16 +17,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   picker: {
-    width: 100,
-  },
+    width: 100
+  }
 });
 
 export default class App extends Component {
@@ -27,12 +36,26 @@ export default class App extends Component {
     this.handleAppStateChange = this.handleAppStateChange.bind(this);
     this.state = {
       seconds: 5,
-      question: "what's the hot new thing?"
+      question: "what's the hot new thing?",
+      text: '',
+      placeholder: 'the thing',
+      editDate: ''
     };
   }
 
   componentDidMount() {
     AppState.addEventListener('change', this.handleAppStateChange);
+
+    let date = new Date(Date.now() + this.state.seconds * 1000);
+
+    if (Platform.OS === 'ios') {
+      date = date.toISOString();
+    }
+
+    PushNotification.localNotificationSchedule({
+      message: this.state.question,
+      date
+    });
   }
 
   componentWillUnmount() {
@@ -41,41 +64,67 @@ export default class App extends Component {
 
   handleAppStateChange(appState) {
     if (appState === 'background') {
-      let date = new Date(Date.now() + (this.state.seconds * 1000));
-
+      let date = new Date(Date.now() + this.state.seconds * 1000);
+      this.setState({ editDate: date.toISOString });
       if (Platform.OS === 'ios') {
         date = date.toISOString();
       }
 
-      PushNotification.localNotification({
-        message: "A message right away"
-      });
+      // PushNotification.localNotification({
+      //   message: 'A message right away'
+      // });
 
-      PushNotification.localNotificationSchedule({
-        message: "My Notification Message",
-        date,
-      });
+      // PushNotification.localNotificationSchedule({
+      //   message: this.state.question,
+      //   date
+      // });
+    }
+  }
+
+  async onEndEditing() {
+    console.log('onEndEditing was called');
+    try {
+      await AsyncStorage.setItem(
+        `@HotNewThings${this.state.editDate}`,
+        `${this.state.text}`
+      );
+    } catch (error) {
+      // Error saving data
+      console.error(error);
+    }
+
+    try {
+      const value = await AsyncStorage.getItem(
+        `@HotNewThings${this.state.editDate}`
+      );
+      if (value !== null) {
+        // We have data!!
+        console.log(value);
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.error(error);
     }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {this.state.question}
-        </Text>
-        <Text style={styles.welcome}>
-          Choose your notification time in seconds.
-        </Text>
-        <Picker
-          style={styles.picker}
-          selectedValue={this.state.seconds}
-          onValueChange={(seconds) => this.setState({ seconds })}
-        >
-          <Picker.Item label="5" value={5} />
-          <Picker.Item label="10" value={10} />
-          <Picker.Item label="15" value={15} />
-        </Picker>
+        <Text style={styles.welcome}>{this.state.question}</Text>
+        <TextInput
+          style={{
+            height: 40,
+            width: 200,
+            borderColor: '#F5FCFF',
+            borderWidth: 1
+          }}
+          onChangeText={text => this.setState({ text })}
+          onEndEditing={this.onEndEditing.bind(this)}
+          editable={true}
+          placeholder={this.state.placeholder}
+          placeholderTextColor={'lightgray'}
+          value={this.state.text}
+        />
         <PushController />
       </View>
     );
